6de4c0be5e4d20ad03280acfdf7affa78ef9a30c
Francisco Rios <francisco.riosgalan@keytree.es>
2019-02-13 18:23:21 +0100

    Add pages and services

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Ionic-Firebase project from scratch
 
 This project is just for learning Firebase with an IONIC 4 project with Angular as JS framework and using Capacitor instead of Cordova
+Is done following this link https://javebratt.com/ionic-firebase-tutorial-intro/ by Jorge Vergara (https://javebratt.com)
+The guide is also available on this same repository BASIC_building-firestore-powered-ionic-apps-2.0.2.pdf
 
 Steps:
 1. npm install (the repo doesn't have node_modules folder)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,12 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'event-create', loadChildren: './pages/event-create/event-create.module#EventCreatePageModule' },
+  { path: 'event-detail', loadChildren: './pages/event-detail/event-detail.module#EventDetailPageModule' },
+  { path: 'event-list', loadChildren: './pages/event-list/event-list.module#EventListPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
 ];
 
 @NgModule({
