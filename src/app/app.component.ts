0cfdb45593e84d1e72f0147ff1895afa12f6be30
Paco Rios <pacoriosgalan@gmail.com>
2019-02-14 18:55:46 +0100

    Setting up Firebase. Prevent anonymous users from accessing pages with Authentication Guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,46 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+//import { Platform } from '@ionic/angular';
 import { Plugins } from '@capacitor/core';
+
+//Firebase
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './credentials';
+
 // import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 // import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-const {SplashScreen, StatusBar } = Plugins;
+const { SplashScreen, StatusBar } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    // private splashScreen: SplashScreen,
-    // private statusBar: StatusBar
-  ) {
+  // constructor(
+  //   private platform: Platform,
+  //   // private splashScreen: SplashScreen,
+  //   // private statusBar: StatusBar
+  // ) {
+  //   firebase.initializeApp();
+  //   this.initializeApp();
+  // }
+
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
     this.initializeApp();
   }
 
   initializeApp() {
     SplashScreen.hide().catch(error => {
-      console.error(error);
+      console.error('Error hiding SplashScreen: ',error);
     });
 
     StatusBar.hide().catch(error => {
-      console.error(error);
+      console.error('Error hiding StatusBar: ',error);
     });
   }
 
+  // Is this from Cordova???
   // initializeApp() {
   //   this.platform.ready().then(() => {
   //     // this.statusBar.styleDefault();
